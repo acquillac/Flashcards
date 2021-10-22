5ca68eab57beee7e1cc6f20bf489a9cd35916abf
acquillac <acquillaclarke@live.ca>
2021-10-22 17:57:18 -0600

    adding cards

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const openForm = document.querySelector('.question-btn'),
       questionForm = document.querySelector('.question-form'),
-      showAnswer = document.querySelectorAll('.show-hide'),
-      deleteCard = document.querySelector('.delete-card');
+      addNewCard = document.querySelector('.save'),
+      questionContent = document.querySelector('.input-question'),
+      answerContent = document.querySelector('.input-answer');
       
 
 // open/close question form
@@ -18,17 +19,49 @@ openForm.addEventListener('click', (e) => {
   
 })
 
-// show/hide answer
-showAnswer.forEach((question) => {
 
-  question.addEventListener('click', (e) => {
-    const answer = e.target.nextElementSibling;
+// add new card
+addNewCard.addEventListener('click', () => {
   
-    if ( answer.style.display === 'block' ){
+  const questionInput = questionContent.value;
+  const answerInput = answerContent.value;
+
+  let output = `
+    <div class="flash-card">
+      <h3 class="display-question">${questionInput}</h3>
+      <a class="show-hide" href="#">Show/hide Answer</a>
+      <p class="answer">${answerInput}</p>
+      <button class="delete-card">Delete</button>
+    </div>
+  `;
+
+  const flashcards = document.querySelector('.flashcards');
+  flashcards.innerHTML += output;
+
+
+  // showing/hiding answer
+  const allShowHide = document.querySelectorAll('.show-hide');
+
+  allShowHide.forEach((question) => {
+    
+    question.addEventListener('click', (e) => {
+      const answer = e.target.nextElementSibling;
+
+      if (answer.style.display === 'block') {
         answer.style.display = 'none';
-    }
-    else{
+      }
+      else {
         answer.style.display = 'block';
-    }
+      }
+    })
+  })
+
+  // adding delete button
+  const allDeleteButons = document.querySelectorAll('.delete-card');
+
+  allDeleteButons.forEach((question) => {
+    question.addEventListener('click', (e) => {
+      e.target.parentElement.remove();
+    })
   })
 })
