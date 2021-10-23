b5ab5abf261058ffc11b0caf7876c7ddc7f72536
acquillac <acquillaclarke@live.ca>
2021-10-23 07:40:25 -0600

    error message

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const openForm = document.querySelector('.question-btn'),
       questionForm = document.querySelector('.question-form'),
       addNewCard = document.querySelector('.save'),
       questionContent = document.querySelector('.input-question'),
-      answerContent = document.querySelector('.input-answer');
+      answerContent = document.querySelector('.input-answer'),
+      error = document.querySelector('.error');
       
 
 // open/close question form
@@ -23,45 +24,61 @@ openForm.addEventListener('click', (e) => {
 // add new card
 addNewCard.addEventListener('click', () => {
   
-  const questionInput = questionContent.value;
-  const answerInput = answerContent.value;
+  if ( questionContent.value === '' || answerContent.value === '' ){
+    showError();
+  }else{
 
-  let output = `
+    const questionInput = questionContent.value;
+    const answerInput = answerContent.value;
+
+    let output = `
     <div class="flash-card">
       <h3 class="display-question">${questionInput}</h3>
       <a class="show-hide" href="#">Show/hide Answer</a>
       <p class="answer">${answerInput}</p>
       <button class="delete-card">Delete</button>
     </div>
-  `;
+    `;
 
-  const flashcards = document.querySelector('.flashcards');
-  flashcards.innerHTML += output;
+    const flashcards = document.querySelector('.flashcards');
+    flashcards.innerHTML += output;
 
 
-  // showing/hiding answer
-  const allShowHide = document.querySelectorAll('.show-hide');
+    // showing/hiding answer
+    const allShowHide = document.querySelectorAll('.show-hide');
 
-  allShowHide.forEach((question) => {
-    
-    question.addEventListener('click', (e) => {
-      const answer = e.target.nextElementSibling;
+    allShowHide.forEach((question) => {
 
-      if (answer.style.display === 'block') {
-        answer.style.display = 'none';
-      }
-      else {
-        answer.style.display = 'block';
-      }
+      question.addEventListener('click', (e) => {
+        const answer = e.target.nextElementSibling;
+
+        if (answer.style.display === 'block') {
+          answer.style.display = 'none';
+        }
+        else {
+          answer.style.display = 'block';
+        }
+      })
     })
-  })
 
-  // adding delete button
-  const allDeleteButons = document.querySelectorAll('.delete-card');
+    // adding delete button
+    const allDeleteButons = document.querySelectorAll('.delete-card');
 
-  allDeleteButons.forEach((question) => {
-    question.addEventListener('click', (e) => {
-      e.target.parentElement.remove();
+    allDeleteButons.forEach((question) => {
+      question.addEventListener('click', (e) => {
+        e.target.parentElement.remove();
+      })
     })
-  })
+  }
 })
+
+
+// show error
+function showError() {
+  error.style.display = 'block';
+
+  setTimeout( function () {
+    error.style.display = 'none';
+  },
+  3000)
+}
